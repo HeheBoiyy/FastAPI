f6d4d450396c47c37bfcb36834700b8710cf7390
HeheBoiyy <school152qq@mail.ru>
2024-10-14 02:13:22 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import fastapi_users
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserCreate, UserRead
+
+app = FastAPI(
+    title="MegaApp"
+)
+
+fastapi_users = fastapi_users.FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
